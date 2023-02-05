15f3294d8328e1679ff91e3c2aeb3fb16170bba5
Murtada al Mousawy <murtada.al.mousawy@gmail.com>
2023-02-05 03:30:49 +0100

    Add 404 error response to slug page.

--- src/app/episode/[episodeSlug]/head.tsx
+++ src/app/episode/[episodeSlug]/head.tsx
@@ -1,9 +1,15 @@
+import { notFound } from 'next/navigation';
+
 // Data
 import getEpisodes, { getEpisodeBySlug } from '@/shared/episodeData';
 
 export default async function Head({ params }: { params: any }) {
   const episodes = await getEpisodes();
   const episode = getEpisodeBySlug(episodes, params.episodeSlug);
+
+  if (!episode?.title) {
+    notFound();
+  }
 
   return (
     <>
--- src/app/episode/[episodeSlug]/page.tsx
+++ src/app/episode/[episodeSlug]/page.tsx
@@ -1,14 +1,18 @@
+import { notFound } from 'next/navigation';
+
 // Data
 import getEpisodes, { getEpisodeBySlug } from '@/shared/episodeData';
 
 // Components
-import Footer from '@/components/Global/Footer';
-import Header from '@/components/Global/Header';
 import EpisodeSingle from '@/components/Episode/EpisodeSingle';
 
 export default async function EpisodeSlug({ params }: { params: any }) {
   const episodes = await getEpisodes();
   const episode = getEpisodeBySlug(episodes, params.episodeSlug);
+
+  if (!episode?.title) {
+    notFound();
+  }
 
   return (
     <>
